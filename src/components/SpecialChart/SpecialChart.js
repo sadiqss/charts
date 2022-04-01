bc610b1a0d8fa4670b0dfef90994bcc09e1de40e
sadiqss <sadiq.sau48@gmail.com>
2022-04-01 12:37:47 +0100

    Axios use

---
+++ src/components/SpecialChart/SpecialChart.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
+
+const SpecialChart = () => {
+    const [phones, setPhones] = useState([]);
+    useEffect(() => {
+        axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
+            .then(data => {
+                const loadedData = data.data.data;
+                const phoneData = loadedData.map(phone => {
+                    const parts = phone.slug.split('-');
+                    const ph = {
+                        name: parts[0],
+                        value: parseInt(parts[1])
+                    };
+                    return ph;
+                });
+                setPhones(phoneData);
+            })
+    }, [])
+    return (
+        <BarChart width={750} height={400} data={phones}>
+            <Bar dataKey="value" fill="#8884d8" />
+            <XAxis dataKey={'name'}></XAxis>
+            <YAxis></YAxis>
+            <Tooltip></Tooltip>
+        </BarChart>
+    );
+};
+
+export default SpecialChart;
